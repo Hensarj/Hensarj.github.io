c8712138b348d722f073973b0c63a721526b301f
Олег Абаляев <oleg@MacBook-Pro-Oleg.local>
2018-11-18 18:42:41 +0300

    love nadezhda

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const openButton = document.querySelector(".hamburger-menu-link");
 const overlay = document.querySelector(".overlay");
 const bars = document.querySelector(".hamburger-menu-link__bars");
 const closeButton = document.querySelector('.hamburger-menu-link');
+const menuLink = document.querySelectorAll('.overlay__item a');
 
 openButton.addEventListener('click', function (e) {
     e.preventDefault();
@@ -12,3 +13,14 @@ openButton.addEventListener('click', function (e) {
         overlay.style.display = 'flex';
     };
 });
+
+for (var i = 0; i < menuLink.length; i++) {
+    menuLink[i].addEventListener('click', function () {
+        bars.classList.toggle('hamburger-menu-link--crossed');
+        if (overlay.style.display === 'flex') {
+            overlay.style.display = 'none';
+        } else {
+            overlay.style.display = 'flex';
+        };
+    });
+}
